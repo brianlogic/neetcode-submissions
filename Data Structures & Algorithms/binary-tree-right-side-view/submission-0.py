ad1c9d2ee5c5b49216abd4d72595f1806d9f6f38
Brian Tran <124115003+brianlogic@users.noreply.github.com>
2026-09-24 18:49:40 -0400

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # prioritize right side elements
+        # we could solve by creating level order traversal and selecting [-1] from each level
+        result = []
+        def helper(root, i): 
+            if root is None:
+                return
+            else: 
+                if len(result) <= i: 
+                    result.append([])
+                result[i].append(root.val)
+                helper(root.left, i + 1)
+                helper (root.right, i + 1)
+        helper(root, 0)
+        r = []
+        for level in result: 
+            r.append(level[-1])
+        return r
+        
+        
